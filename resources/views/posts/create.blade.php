1b1e05918256d7d35a6bf90bc9745779a4d93fc8
Admin <admin@Admins-Mac-mini.local>
2025-11-13 22:11:40 +0530

    Styled post views with tailwind and added validation message

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -8,8 +8,16 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
-                    <h1>Create Post</h1>
-
+                    <h1 class="text-3xl my-2">Create Post</h1>
+                    @if ($errors->any())
+                        <div class="mb-4 p-4 rounded-md bg-red-50 border border-red-200">
+                            <ul class="list-disc list-inside text-sm text-red-600">
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
                     <form action="{{ route('posts.store') }}" method="POST">
                         @csrf
                         <p>
@@ -20,7 +28,7 @@
                             <label>Content:</label><br>
                             <textarea name="content">{{ old('content') }}</textarea>
                         </p>
-                        <button type="submit">Save</button>
+                        <button type="submit" class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 my-2 px-4 rounded shadow-lg">Save</button>
                     </form>
                 </div>
             </div>
